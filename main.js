d43dc7ae7b0b904e43789843c1bd3d4775446b2c
Hiroshi Tei <zhhaozhi0717@gmail.com>
2022-09-17 23:07:14 +0900

    フォルダ構成を変更

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-const element = document.querySelectorAll("tr.seiseki"); // 成績に関するすべての情報
-var total_earned_credits = 0
-for (let i = 6; i < element.length - 1; i++){
-
-    const child = element[i].querySelectorAll("td") // 履修している科目
-    const credit = Number(child[6].textContent) // 各科目の単位数
-    const grade = child[7].textContent // 成績評価
-    const class_type = child[8].textContent  // 科目区分
-    const passed = child[12].textContent.trim() // 合否 合なら単位取得
-    if (passed != "合") {
-        continue
+function get_grades_infomation(){
+    const element = document.querySelectorAll("tr.seiseki"); // 成績に関するすべての情報
+    const gakka_name = element[1].querySelectorAll("td")[0].textContent;
+    var seiseki_info = [];
+    for (let i = 6; i < element.length - 1; i++){
+        const subject = element[i].querySelectorAll("td"); // 履修している科目
+        const credit = Number(subject[6].textContent); // 各科目の単位数
+        const grade = subject[7].textContent; // 成績評価
+        const subject_type = subject[8].textContent.trim();  // 科目区分
+        const passed = subject[12].textContent.trim(); // 合否 合なら単位取得
+        const subject_info = [credit, grade, subject_type, passed];
+        seiseki_info.push(subject_info);
     }
-
-    total_earned_credits = total_earned_credits + credit
-
-    // console.log(credit)
-    // console.log(typeof(credit))
-    // console.log(grade)
-    // console.log(class_type)
-    // console.log(passed)
+    return [gakka_name, seiseki_info]
 }
-
-console.log(total_earned_credits)
